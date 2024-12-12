4ff4a05c4a0579aadc7272bc1696a69e13f4f787
Noriel Navarro <norieljon.nvr@gmail.com>
2024-12-12 18:21:43 -0500

    Added a reset follower count functionality

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,13 +6,22 @@ function increaseCount() {
     checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+    count = 0; // Reset the count back to 0
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
 function checkCountValue() {
     if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
+    } else if (count === 0) {
+        alert("Your followers count has been reset back to 0");
     }
 }
+
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
