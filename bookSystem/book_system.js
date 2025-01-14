6947d8c1d9e4884c484a8106b4fc0d04cf3ba8c5
Noriel Navarro <norieljon.nvr@gmail.com>
2025-01-13 22:01:44 -0500

    Updated Book System

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -22,12 +22,18 @@ function addBook() {
     }
 }
 
+function deleteBook(bookId) {
+    books.splice(bookId, 1);
+    showbooks();
+}
+
 function showbooks() {
     const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete Book</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
